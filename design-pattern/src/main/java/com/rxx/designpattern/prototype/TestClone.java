b9e2c0a3d7c11aa31c0a3813365042e7377b0786
zhang <zd_register@126.com>
2018-01-15 17:50:08 +0800

    修改深拷贝与浅拷贝的测试case

--- design-pattern/src/main/java/com/rxx/designpattern/prototype/TestClone.java
+++ design-pattern/src/main/java/com/rxx/designpattern/prototype/TestClone.java
@@ -14,15 +14,21 @@ public class TestClone {
 	public static void main(String[] args) throws CloneNotSupportedException, ClassNotFoundException, IOException {
 		ConcretePrototype pro1 = new ConcretePrototype(new String("prototype"), new SerializableObject());
 		ConcretePrototype pro2 = (ConcretePrototype) pro1.clone();
-		System.out.println(pro1.getName());		
-		System.out.println(pro1.getObj());		
-		System.out.println(pro2.getName());		
-		System.out.println(pro2.getObj());		
+		System.out.println("pro1.getName():" + pro1.getName());
+		System.out.println("pro1.getObj():" + pro1.getObj());
+		System.out.println("pro2.getName():" + pro2.getName());
+		System.out.println("pro2.getObj():" + pro2.getObj());
+		System.out.println(pro1.getName() == pro2.getName());
+		System.out.println(pro1.getObj() == pro2.getObj());
+
+		System.out.println("=========================================================");
 		ConcretePrototype pro3 = new ConcretePrototype(new String("deep prototype"), new SerializableObject());
 		ConcretePrototype pro4 = (ConcretePrototype) pro3.deepClone();
-		System.out.println(pro3.getName());		
-		System.out.println(pro3.getObj());		
-		System.out.println(pro4.getName());		
-		System.out.println(pro4.getObj());		
+		System.out.println("pro3.getName():" + pro3.getName());
+		System.out.println("pro3.getObj():" + pro3.getObj());
+		System.out.println("pro4.getName():" + pro4.getName());
+		System.out.println("pro4.getObj():" + pro4.getObj());
+		System.out.println(pro3.getName() == pro4.getName());
+		System.out.println(pro3.getObj() == pro4.getObj());
 	}
 }
